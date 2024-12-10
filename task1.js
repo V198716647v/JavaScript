0bc9fa594f0e75053bb651cd0018c7a9daeb2a98
Kirill <They@example.com>
2024-12-10 20:05:35 +0300

    5 задание

--- task1.js
+++ task1.js
@@ -8,11 +8,18 @@ let result = 2 + 6 / (1 + 3);
 console.log(result)
 
 
-const prompt = require('prompt-sync')()
-let x = prompt('введите число:')
+const prompt = require('prompt-sync')();
+let x = prompt('введите число:');
 if (x>0 && x<9){
     console.log('истина')
 } else {
-    console.log('дожь')
+    console.log('ложь')
 }
 
+
+let a = prompt('введите число:')
+if (a%--0) {
+    console.log('истина')
+} else {
+    console.log('ложь')
+}
