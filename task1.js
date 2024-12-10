0d68278c722ac3e4361348cdd035c19005403f5b
Kirill <They@example.com>
2024-12-10 19:54:20 +0300

    4 задание

--- task1.js
+++ task1.js
@@ -1,7 +1,18 @@
 let maximumDataTransferSpeed = 100;
 
+
 let dayOfWeek = 7;
+
 
 let result = 2 + 6 / (1 + 3);
 console.log(result)
+
+
+const prompt = require('prompt-sync')()
+let x = prompt('введите число:')
+if (x>0 && x<9){
+    console.log('истина')
+} else {
+    console.log('дожь')
+}
 
