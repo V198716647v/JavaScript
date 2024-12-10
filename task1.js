eec6d41c28bd1c960ca6746bf38298355d882c19
Kirill <They@example.com>
2024-12-10 20:31:06 +0300

    6 задача

--- task1.js
+++ task1.js
@@ -17,9 +17,16 @@ if (x>0 && x<9){
 }
 
 
-let a = prompt('введите число:')
-if (a%--0) {
+let a = prompt('введите число от 0 до 10: ');
+if (a % 2 ==0) {
     console.log('истина')
 } else {
     console.log('ложь')
 }
+
+let sum1 = 1 + true;
+console.log(sum1, typeof sum1);
+let sum2 = '1' + 2;
+console.log(sum2, typeof sum2);
+let sum3 = '1' + false;
+console.log(sum3,typeof sum3);
