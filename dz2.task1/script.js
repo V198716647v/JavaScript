7d35d9f3b0e115b2f4225caa58d585f98d469306
Your Name <valeriya9169566@gmail.com>
2024-12-22 19:56:59 +0300

    task1

--- dz2.task1/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    if (confirm('вы уверены, что хотите продолжить?')) {
-        alert('подтверждено') 
-    } else {
-        alert('отменено')
-    }
-});
